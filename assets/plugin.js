5ad88dee3e6c859b641d7bda53fc77c2d0231f27
Zhengjia Wang <zhengjia.wang@nxp.com>
2020-09-13 19:40:47 +0800

    Read versions from remote file

--- assets/plugin.js
+++ assets/plugin.js
@@ -19,28 +19,31 @@ require(['gitbook', 'jQuery'], function (gitbook, $) {
         });
         var $select = $li.find('select');
 
-        $.each(versions, function(i, version) {
+        $.each(versions, function (i, version) {
             var $option = $('<option>', version);
             $option.appendTo($select);
         });
-        $select.find("option[value='"+current_version+"']").attr("selected", true)
+        $select.find("option[value='" + current_version + "']").attr("selected", true)
 
-        $select.change(function() {
-            var filtered = $.grep(versions, function(v) {
+        $select.change(function () {
+            var filtered = $.grep(versions, function (v) {
                 return v.value === $select.val();
             });
             // Get actual version Object from array
             var version = filtered[0];
-            
+
             window.location.replace(host + '/' + version.value + '/')
         });
 
         $li.prependTo('.book-summary ul.summary');
     }
 
+
     gitbook.events.bind('start', function (e, config) {
         pluginConfig = config.versions || {};
-        if (pluginConfig.options) updateVersions(pluginConfig.options);
+        $.getJSON(pluginConfig.config, function (data) {
+            updateVersions(data);
+        });
     });
 
     gitbook.events.bind('page.change', function () {
